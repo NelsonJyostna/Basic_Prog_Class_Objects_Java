7f764b7d62e36aa872c853adeefb0f020425028c
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-06-11 17:49:52 +0530

    Added : Name And Roll no

---
+++ Student.java
@@ -0,0 +1,16 @@
+
+
+class Student1 {
+        String name;
+        int roll_no;
+
+}
+ public class Student  {
+     public static void main (String[ ] args) {
+      Student1 s = new Student1();
+      s.name = "John";
+      s.roll_no = 2;
+      System.out.println("Name is "+s.name+" and roll number is "+s.roll_no);
+     }
+ }
+
